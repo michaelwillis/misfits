3e58159db1ebffb730e0c71dbd852ce609c9299d
Michael Willis <willismichael@gmail.com>
2014-07-24 21:22:28 -0500

    pit tiles

--- desktop/src-common/misfits/core.clj
+++ desktop/src-common/misfits/core.clj
@@ -6,10 +6,10 @@
 (def tile-size 32)
 
 (def level ["HHHHHH HHHHHHH H"
+            "H   HO   OH     "
+            "H   O           "
             "H   H     H     "
-            "H               "
-            "H   H     H     "
-            "HH HH     H     "
+            "HHOHH     H     "
             "H   HHHHHHHHH HH"
             "H        H      "
             "         H      "
@@ -23,9 +23,10 @@
             "H   H       H   "])
 
 (def floor-map (into {} (for [x (range 0 64) y (range 0 64)]
-                          [[x y] (if (= \space (-> level (nth (mod x 16))
-                                                   (nth (mod y 16))))
-                                   :floor :wall)])))
+                          [[x y] (case (-> level (nth (mod x 16)) (nth (mod y 16)))
+                                   \O :pit
+                                   \H :wall
+                                   :floor)])))
 
 (defn resize-pixels! [screen]
   (let [pixel-size (if (< 768 (game :height)) 3 2)]
@@ -41,28 +42,30 @@
           stone1 (texture (aget tiles-textures 0 0))
           stone2 (texture (aget tiles-textures 0 0))
           stone3 (texture (aget tiles-textures 0 0))
-          wall-bottom (texture (aget tiles-textures 0 1))
-          wall-corner (texture (aget tiles-textures 0 2))
-          wall-top (texture (aget tiles-textures 0 3))
-          ]
+          wall-front (texture (aget tiles-textures 0 1))
+          wall-top (texture (aget tiles-textures 0 2))
+          wall-back (texture (aget tiles-textures 0 3))
+          pit (texture (aget tiles-textures 1 0))]
 
       (resize-pixels! screen)
 
       (position! screen (* 16 tile-size) (* 16 tile-size))
 
       (flatten
-      (for [x (range 0 128) y (range 0 128)]
-        (let [this-tile (floor-map [x y])
-              up-tile (floor-map [x (inc y)])
-              down-tile (floor-map [x (dec y)])
-              tile (cond (and (= this-tile :wall) (= down-tile :floor)) wall-corner
-                              (= this-tile :wall) wall-top
-                              (= up-tile :wall) wall-bottom
-                         :default (rand-nth [stone1 stone1 stone1 stone1
-                                             stone1 stone2 stone2 stone3]))]
-          [(assoc tile :x (* tile-size x) :y (* y tile-size))]))
-      )))
-
+       (for [x (range 0 128) y (range 0 128)]
+         (let [this-tile (floor-map [x y])
+               up-tile (floor-map [x (inc y)])
+               down-tile (floor-map [x (dec y)])
+               
+               results [(cond (= this-tile :floor) stone1
+                               (= this-tile :wall) (if (= down-tile :wall) wall-top wall-front)
+                               (= this-tile :pit) pit)]
+               results (if (and (not= this-tile :wall) (= down-tile :wall))
+                         (conj results wall-back)
+                         results)]
+           
+           (map #(assoc % :x (* tile-size x) :y (* y tile-size)) results))))))
+  
   :on-render
   (fn [screen entities]
     (clear!)
